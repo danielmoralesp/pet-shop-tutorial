1e2a8f40542332cf74723f2b8bd7e773255b2315
danielmoralesp <daniel_moralesp@hotmail.com>
2017-12-12 17:48:45 -0500

    Solve error. Its .json not .js. The app is done

--- src/js/app.js
+++ src/js/app.js
@@ -25,8 +25,7 @@ App = {
 
   initWeb3: function() {
     // Is there is an injected web3 instance?
-
-    if (typeof web3 !== 'undefined'){
+    if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
     } else {
       // If no injected web3 instance is detected, fallback to the TestRPC
@@ -38,7 +37,7 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('Adoption.js', function(data){
+    $.getJSON('Adoption.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract
       var AdoptionArtifact = data;
       App.contracts.Adoption = TruffleContract(AdoptionArtifact);
@@ -60,17 +59,17 @@ App = {
   markAdopted: function(adopters, account) {
     var adoptionInstance;
 
-    App.contracts.Adoption.deployed().then(function(instance){
+    App.contracts.Adoption.deployed().then(function(instance) {
       adoptionInstance = instance;
 
       return adoptionInstance.getAdopters.call();
-    }).then(function(adopters){
-      for (i=0; i < adopters.length; i++){
-        if (adopters[i] !== '0x0000000000000000000000000000000000000000'){
+    }).then(function(adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
           $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
         }
       }
-    }).catch(function(err){
+    }).catch(function(err) {
       console.log(err.message);
     });
   },
@@ -82,21 +81,21 @@ App = {
 
     var adoptionInstance;
 
-    web3.eth.getAccounts(function(error, accounts){
-      if (error){
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
         console.log(error);
       }
 
       var account = accounts[0];
 
-      App.contracts.Adoption.deployed().then(function(instance){
+      App.contracts.Adoption.deployed().then(function(instance) {
         adoptionInstance = instance;
 
         // Execute adopt as a transaction by sending account
         return adoptionInstance.adopt(petId, {from: account});
-      }).then(function(result){
+      }).then(function(result) {
         return App.markAdopted();
-      }).catch(function(err){
+      }).catch(function(err) {
         console.log(err.message);
       });
     });
